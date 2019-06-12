874a3c877c72e62c8ef038a657b4b1f75a195621
bokasa <milos.ruzic.52.14@ict.edu.rs>
2019-06-12 23:13:02 +0200

    added user dto

---
+++ Application/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTO
+{
+    public class UserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<DTOComment> Comments { get; set; }
+        public ICollection<DTOAd> Ads { get; set; }
+    }
+}
